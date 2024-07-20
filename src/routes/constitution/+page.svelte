31c57b54360e38e1423293788a324985c5d56c61
jsteinfink914 <jsteinfink914@gmail.com>
2024-07-20 10:50:23 -0400

    Styling

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -18,6 +18,7 @@
     width="100%"
     height="600px"
     style="border: none;"
+    style="overflow: auto; display: block;"
   >
     This browser does not support PDFs. Please download the PDF to view it: <a href="/constitution.pdf">Download PDF</a>.
   </iframe>
