6fdacc9e907f793200caa0ffde003456c7232847
jsteinfink914 <76917720+jsteinfink914@users.noreply.github.com>
2025-06-14 19:34:55 -0400

    Update +page.svelte

--- src/routes/team_values/+page.svelte
+++ src/routes/team_values/+page.svelte
@@ -13,7 +13,7 @@
   const fetchData = async () => {
     try {
       // Fetch rosters
-      const rosterResponse = await fetch('https://api.sleeper.app/v1/league/1053526624240517120/rosters');
+      const rosterResponse = await fetch('https://api.sleeper.app/v1/league/1192332341500989440/rosters');
       const rosters = await rosterResponse.json();
 
       // Fetch players
